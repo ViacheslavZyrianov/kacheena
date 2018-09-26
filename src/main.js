1b5c3bc234583e1945b76c7d917454e11f5517da
ViacheslavZyrianov <zyrianov.viacheslav@gmail.com>
2018-09-27 00:28:08 +0300

    register global component fa-icon

--- src/main.js
+++ src/main.js
@@ -3,9 +3,13 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+
 import Vuetify from 'vuetify'
 import 'vuetify/dist/vuetify.min.css'
 import firebase from './utils/firebase'
+
+import faIcon from '@/components/shared/faIcon'
+Vue.component('fa-icon', faIcon)
 
 Vue.use(Vuetify)
 Vue.use(firebase)
