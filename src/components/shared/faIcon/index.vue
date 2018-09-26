eb6691224da2b34a943a7535dc436f7be4d60127
ViacheslavZyrianov <zyrianov.viacheslav@gmail.com>
2018-09-27 00:27:39 +0300

    create global component fa-icon

---
+++ src/components/shared/faIcon/index.vue
@@ -0,0 +1,26 @@
+<template>
+  <v-icon>{{ iconName }}</v-icon>
+</template>
+
+<script>
+  import '@fortawesome/fontawesome-free/css/all.css'
+  export default {
+    name: 'FaIcon',
+    props: {
+      name: {
+        default: '',
+        type: String,
+        required: true
+      }
+    },
+    computed: {
+      iconName () {
+        return `fas fa-${this.name}`
+      }
+    }
+  }
+</script>
+
+<style lang="scss" scoped>
+</style>
+
