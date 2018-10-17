7c955f95e9bc64b1d108bfb19b06477726c126ff
ViacheslavZyrianov <zyrianov.viacheslav@gmail.com>
2018-10-17 23:31:18 +0300

    add layout and functionality for authorization

--- src/components/pages/auth/index.vue
+++ src/components/pages/auth/index.vue
@@ -1,3 +1,69 @@
 <template>
-  <fa-icon name="lemon" />
+  <v-card
+    width="400"
+  >
+    <div class="auth__phone">
+      <v-text-field
+        placeholder="Phone"
+        v-model="phoneNumber"
+      />
+      <div id="recaptcha-container"></div>
+      <v-btn
+        @click="onSendVerificationCode"
+        :disabled="isAuthorizeViaPhoneBtnDisabled"
+      >
+        Send verification code
+      </v-btn>
+      <v-text-field
+        placeholder="Verification code"
+        v-model="verificationCode"
+      />
+      <v-btn
+        @click="onAuthorize"
+      >
+        Authorize
+      </v-btn>
+    </div>
+  </v-card>
 </template>
+
+<script>
+  import { mapActions } from 'vuex'
+
+  // import { initRecaptcha } from '@/utils/recaptcha'
+
+  export default {
+    name: 'pageAuth',
+    data () {
+      return {
+        phoneNumber: '',
+        verificationCode: ''
+      }
+    },
+    methods: {
+      ...mapActions({
+        initRecaptcha: 'authPhone/initRecaptcha',
+        sendVerificationCode: 'authPhone/sendVerificationCode',
+        authorize: 'authPhone/authorize'
+      }),
+      onSendVerificationCode () {
+        this.sendVerificationCode(this.phoneNumber)
+      },
+      onAuthorize () {
+        this.authorize(this.verificationCode)
+      }
+    },
+    computed: {
+      isAuthorizeViaPhoneBtnDisabled () {
+        return false
+      }
+    },
+    mounted () {
+      this.initRecaptcha()
+    }
+  }
+</script>
+
+<style lang="scss" scoped>
+</style>
+
