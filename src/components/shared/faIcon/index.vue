001852f658a1d91cc3a99afd36ecb372289417a0
ViacheslavZyrianov <zyrianov.viacheslav@gmail.com>
2018-11-10 21:36:45 +0200

    modify component faIcon

--- src/components/shared/faIcon/index.vue
+++ src/components/shared/faIcon/index.vue
@@ -1,9 +1,15 @@
 <template>
-  <v-icon>{{ iconName }}</v-icon>
+  <v-icon
+    :style="{color: color}"
+    left
+  >
+    {{ iconName }}
+  </v-icon>
 </template>
 
 <script>
   import '@fortawesome/fontawesome-free/css/all.css'
+  import '@fortawesome/fontawesome-free/css/brands.css'
   export default {
     name: 'FaIcon',
     props: {
@@ -11,11 +17,23 @@
         default: '',
         type: String,
         required: true
+      },
+      color: {
+        default: '#fff',
+        type: String
+      },
+      type: {
+        default: '',
+        type: String
+      },
+      side: {
+        default: 'left',
+        type: String
       }
     },
     computed: {
       iconName () {
-        return `fas fa-${this.name}`
+        return `fa ${this.type} fa-${this.name}`
       }
     }
   }
