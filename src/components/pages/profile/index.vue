a1ba8eb867712ae4f4d37f0b15f5fef1aa9a011f
ViacheslavZyrianov <zyrianov.viacheslav@gmail.com>
2018-11-25 23:53:07 +0200

    create profile page

---
+++ src/components/pages/profile/index.vue
@@ -0,0 +1,30 @@
+<template>
+  <section class="page page-profile">
+    <h1>Profile</h1>
+    <h2>Name: {{ getUser.displayName }}</h2>
+    <img
+      :src="getUser.photoURL"
+      :alt="getUser.displayName"
+    >
+  </section>
+</template>
+
+<script>
+import { mapGetters } from 'vuex'
+
+export default {
+  name: 'PageProfile',
+  computed: {
+    ...mapGetters({
+      getUser: 'getUser'
+    })
+  }
+}
+</script>
+
+<style lang="scss">
+  html,
+  body {
+      overscroll-behavior-y: contain;
+  }
+</style>
