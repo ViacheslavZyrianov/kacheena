c55d1a70acf13a11452f7a3e9a1a175db0c42c6e
ViacheslavZyrianov <zyrianov.viacheslav@gmail.com>
2018-11-25 22:55:47 +0200

    use snackbar component

--- src/App.vue
+++ src/App.vue
@@ -3,12 +3,17 @@
     <v-content>
       <router-view/>
     </v-content>
+    <snackbar />
   </v-app>
 </template>
 
 <script>
+import snackbar from '@/components/shared/snackbar'
 export default {
-  name: 'App'
+  name: 'App',
+  components: {
+    snackbar
+  }
 }
 </script>
 
