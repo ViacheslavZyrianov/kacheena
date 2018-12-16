9a892c0a51c36d6ba56b2d7ec0dc68f3e98f0f40
ViacheslavZyrianov <zyrianov.viacheslav@gmail.com>
2018-12-16 06:32:30 +0200

    create shared component Avatar

---
+++ src/components/shared/avatar/index.vue
@@ -0,0 +1,57 @@
+<template>
+  <div class="avatar">
+    <img
+      :src="src"
+      :alt="alt"
+      class="avatar__img"
+    >
+  </div>
+</template>
+
+<script>
+import defaultAvatar from '@/assets/img/default-avatar.svg'
+
+export default {
+  name: 'Avatar',
+  props: {
+    src: {
+      type: String,
+      required: true,
+      default: defaultAvatar
+    },
+    alt: {
+      type: String,
+      required: true,
+      default: 'Image'
+    },
+    isEditable: {
+      type: Boolean,
+      default: false
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+$maxAvatarSize: 400px;
+$avatarSize: 60vw;
+
+.avatar{
+    overflow: hidden;
+    border-radius: 50%;
+    width: $avatarSize;
+    height: $avatarSize;
+    max-width: $maxAvatarSize;
+    max-height: $maxAvatarSize;
+
+  &__img {
+    width: $avatarSize;
+    height: $avatarSize;
+    max-width: $maxAvatarSize;
+    max-height: $maxAvatarSize;
+    display: block;
+    object-fit: cover;
+  }
+}
+</style>
+
