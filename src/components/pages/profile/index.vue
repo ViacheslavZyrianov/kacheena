eca5a7cc78721a57f9e132ba7a6af721d7d0c2e8
ViacheslavZyrianov <zyrianov.viacheslav@gmail.com>
2018-12-16 06:31:47 +0200

    completely rewrite PageProfile

--- src/components/pages/profile/index.vue
+++ src/components/pages/profile/index.vue
@@ -1,49 +1,168 @@
 <template>
-  <section class="page page-profile">
-    <img
-      :src="getUser.photoURL"
-      :alt="getUser.displayName"
-      class="profile__photo"
-    >
-    <h1 class="profile__name">{{ getUser.displayName }}</h1>
-  </section>
+  <v-container fill-height>
+    <v-layout column>
+      <avatar
+        v-if="getUser.photoURL"
+        :src="getUser.photoURL"
+        alt="User avatar"
+        class="profile__avatar"
+      />
+      <v-btn @click="onChangePhoto">Change photo</v-btn>
+      <v-layout
+        row align-center shrink
+        :justify-space-between="getUser.displayName"
+        :justify-center="!getUser.displayName"
+      >
+        <template v-if="getUser.displayName">
+          <h1 class="profile__name">{{ this.getUser.displayName }}</h1>
+          <v-btn
+            color="info"
+            @click="onShowChangeDisplayNameDialog"
+          >
+            Edit
+          </v-btn>
+        </template>
+        <v-btn
+          v-else
+          color="info"
+          @click="onShowChangeDisplayNameDialog"
+        >
+          Add name
+        </v-btn>
+      </v-layout>
+      <v-layout row justify-space-between align-center shrink>
+        <v-subheader>Phone</v-subheader>
+        <template v-if="getUser.phoneNumber">
+          <span>{{ getUser.phoneNumber }}</span>
+          <v-btn
+            color="info"
+            @click="onShowChangeProfilePhoneDialog"
+          >
+            Edit
+          </v-btn>
+        </template>
+        <v-btn
+          v-else
+          color="info"
+          @click="onShowChangeProfilePhoneDialog"
+        >
+          Add
+        </v-btn>
+      </v-layout>
+      <v-layout
+        justify-end shrink
+        mt-auto
+      >
+        <v-btn @click="signOut">
+          Sign out
+        </v-btn>
+      </v-layout>
+    </v-layout>
+    <name-change-dialog
+      :name="getUser.displayName"
+      :isDisplayNameDialogVisible="isDisplayNameDialogVisible"
+      @closeDialog="onCancelDialog"
+    />
+    <phone-change-dialog
+      :phone="getUser.phoneNumber"
+      :isProfilePhoneDialogVisible="isProfilePhoneDialogVisible"
+      @closeDialog="onCancelDialog"
+    />
+    <photo-change-dialog
+      v-if="getUser.photoURL"
+      :photoUrl="getUser.photoURL"
+      :isDisplayPhotoDialogVisible="isDisplayPhotoDialogVisible"
+      @closeDialog="onCancelDialog"
+    />
+  </v-container>
 </template>
 
 <script>
-import { mapGetters } from 'vuex'
+import { mapGetters, mapActions } from 'vuex'
+
+import nameChangeDialog from './nameChangeDialog'
+import phoneChangeDialog from './phoneChangeDialog'
+import photoChangeDialog from './photoChangeDialog'
 
 export default {
   name: 'PageProfile',
+  components: {
+    phoneChangeDialog,
+    nameChangeDialog,
+    photoChangeDialog
+  },
+  data () {
+    return {
+      userDisplayName: '',
+      isDisplayNameDialogVisible: false,
+      isProfilePhoneDialogVisible: false,
+      isDisplayPhotoDialogVisible: false
+    }
+  },
   computed: {
     ...mapGetters({
       getUser: 'getUser'
-    })
+    }),
+    avatarAlt () {
+      return `${this.userDisplayName || 'User'}'s avatar`
+    }
+  },
+  created () {
+    this.fetchPhotoUrl()
+  },
+  methods: {
+    ...mapActions({
+      signOut: 'profile/signOut',
+      fetchPhotoUrl: 'profile/fetchPhotoUrl'
+    }),
+    onShowChangeProfilePhoneDialog (profileItem) {
+      this.isProfilePhoneDialogVisible = true
+    },
+    onShowChangeDisplayNameDialog () {
+      this.isDisplayNameDialogVisible = true
+    },
+    onChangePhoto () {
+      this.isDisplayPhotoDialogVisible = true
+    },
+    onCancelDialog () {
+      this.isProfilePhoneDialogVisible = false
+      this.isDisplayNameDialogVisible = false
+      this.isDisplayPhotoDialogVisible = false
+    }
   }
 }
 </script>
 
 <style lang="scss" scoped>
-.page-profile {
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: flex-start;
-}
-
 .profile {
-  &__photo {
-    width: 60vw;
-    height: 60vw;
-    max-width: 400px;
-    max-height: 400px;
-    overflow: hidden;
-    border-radius: 50%;
-    object-fit: cover;
+  &__main-info {
+    margin-bottom: 32px;
+  }
+
+  &__avatar {
+    margin: 0 auto 16px;
   }
 
   &__name {
+    text-align: center;
+    font-size: 32px;
+    font-weight: 300;
     word-break: break-all;
+  }
+
+  &__info-item {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    width: 100vw;
+
+    span {
+      text-overflow: ellipsis;
+      white-space: nowrap;
+      overflow: hidden;
+      padding: 0 16px;
+      text-align: right;
+    }
   }
 }
 </style>
-
