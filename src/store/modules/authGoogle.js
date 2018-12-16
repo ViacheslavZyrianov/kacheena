07081fbe4e367ef0436e6fafd09bca7f68b5027b
ViacheslavZyrianov <zyrianov.viacheslav@gmail.com>
2018-12-16 06:43:02 +0200

    add uid

--- src/store/modules/authGoogle.js
+++ src/store/modules/authGoogle.js
@@ -7,13 +7,12 @@ export const actions = {
       const provider = new firebase.auth.GoogleAuthProvider()
       firebase.auth().useDeviceLanguage()
       firebase.auth().signInWithPopup(provider).then(result => {
-        // This gives you a Google Access Token. You can use it to access the Google API.
-        // var token = result.credential.accessToken
         const userData = {
           displayName: result.user.displayName,
           email: result.user.email,
           photoURL: result.user.photoURL,
-          phoneNumber: result.user.phoneNumber
+          phoneNumber: result.user.phoneNumber,
+          uid: result.user.uid
         }
         commit('setUser', userData, { root: true })
         router.push({name: 'Profile'})
