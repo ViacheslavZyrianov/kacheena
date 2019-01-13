ea74a5415d73b8d698a627c431c68b5d5ef8dc23
ViacheslavZyrianov <zyrianov.viacheslav@gmail.com>
2019-01-13 20:56:44 +0200

    add method update

--- src/utils/ls.js
+++ src/utils/ls.js
@@ -2,6 +2,11 @@ export default {
   set (name, value) {
     localStorage.setItem(name, JSON.stringify(value))
   },
+  update (name, key, val) {
+    const lsData = this.get(name)
+    lsData[key] = val
+    this.set(name, lsData)
+  },
   get (name) {
     return JSON.parse(localStorage.getItem(name))
   },
