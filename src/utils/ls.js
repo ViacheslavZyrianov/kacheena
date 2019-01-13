d3222f48a215325deb432e86ead6982096b2e3e9
ViacheslavZyrianov <zyrianov.viacheslav@gmail.com>
2019-01-13 19:27:19 +0200

    remove return from set and remove in ls.js

--- src/utils/ls.js
+++ src/utils/ls.js
@@ -1,11 +1,11 @@
 export default {
   set (name, value) {
-    return localStorage.setItem(name, JSON.stringify(value))
+    localStorage.setItem(name, JSON.stringify(value))
   },
   get (name) {
     return JSON.parse(localStorage.getItem(name))
   },
   remove (name) {
-    return localStorage.removeItem(name)
+    localStorage.removeItem(name)
   }
 }
