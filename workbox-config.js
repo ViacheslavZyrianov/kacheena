9a5a7dd05d06afacc2b2cdbf78e83f106bcb47ac
ViacheslavZyrianov <zyrianov.viacheslav@gmail.com>
2019-01-13 23:47:09 +0200

    add runtimeCaching and fix workbox-config.js

--- workbox-config.js
+++ workbox-config.js
@@ -1,7 +1,18 @@
-export default {
-  "globDirectory": "dist/",
-  "globPatterns": [
-    "**/*.{html,js,css,woff2,woff,ttf,eot,svg,png,json}"
+module.exports = {
+  globDirectory: 'dist/',
+  globPatterns: [
+    '**/*.{html,js,css,woff2,woff,ttf,eot,svg,png,json}'
   ],
-  "swDest": "dist/service-worker.js"
-};
+  swDest: 'dist/service-worker.js',
+
+  // Define runtime caching rules.
+  runtimeCaching: [{
+    // Match any request ends with .png, .jpg, .jpeg or .svg.
+    urlPattern: /\.(?:png|jpg|jpeg|svg)$/,
+
+    // Apply a cache-first strategy.
+    handler: 'networkFirst',
+
+    options: {}
+  }]
+}
